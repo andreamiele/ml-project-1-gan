b0cd5ce9872ceeb0c80487584c7adbfd86c3b332
jab <gabrieldehame@gmail.com>
2023-10-10 20:59:51 +0200

    hand-made load/store csv files, and training without the cols containing NaN

--- run.py
+++ run.py
@@ -3,31 +3,18 @@ from implementations import *
 
 y = np.genfromtxt("dataset/y_train.csv", delimiter=",", skip_header=1, usecols=0)
 x = np.genfromtxt("dataset/x_train.csv", delimiter=",", skip_header=1)
-ids = x[:, 0].astype(np.int64)
+x_test = np.genfromtxt("dataset/x_test.csv", delimiter=",", skip_header=1)
+x_test = x_test[:, 1:]
 x = x[:, 1:]
-x[np.where(np.isnan(x))] = 0
-y[np.where(y == 0)] = -1
 
-#subsample
-x = x[::50]
-y = y[::50]
-ids = ids[::50]
-
-c = 0
-for a in x:
-  for b in a:
-    if np.isnan(b): c+=1
-for b in y:
-  if np.isnan(b): c += 1
-print(c)
+x_test = x_test[:, np.all(np.logical_not(np.isnan(x)), axis=0)]
+x = x[:, np.all(np.logical_not(np.isnan(x)), axis=0)]
+print(x.shape)
 
 gamma = 0.1
 initial_w = np.zeros(x.shape[1])
 max_iters = 500
-print(x.shape)
-print(y.shape)
-print(initial_w.shape)
 
-losses, ws = mean_squared_error_gd(y, x, initial_w, max_iters, gamma)
-print(ws[:10])
-create_csv_submission(ids, np.dot(x, ws[-1]), "submission.csv")
+w = np.linalg.lstsq(x.T.dot(x), x.T.dot(y))[0]
+print(w)
+np.savetxt("submission.csv", np.dot(x_test,w), header="_MICHD",)
