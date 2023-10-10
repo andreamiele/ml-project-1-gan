3d6b651d3da6cdc0f1be0b50da4ab4aea2df3096
Andrea Miele <andrea.miele.pro@gmail.com>
2023-10-10 21:42:45 +0200

    trying some submission

--- run2.py
+++ run2.py
@@ -1,4 +1,5 @@
 import numpy as np
+import csv
 
 x = np.eye(568238, 2)
 for i in range(568238):
@@ -11,3 +12,11 @@ np.savetxt(
     header="Id, Prediction",
 )
 # Exception: Malformed header of the prediction CSV file. Missing : Prediction
+
+with open("submi", "w") as csvfile:
+    fieldnames = ["Id", "Prediction"]
+    writer = csv.DictWriter(csvfile, delimiter=",", fieldnames=fieldnames)
+    writer.writeheader()
+    for r1 in range(568238):
+        writer.writerow({"Id": int(r1), "Prediction": 0})
+# Exception: All expected Ids were not present in the submission. Please cross check to ensure that the submitted file contains a row each for all the data points in the test set.
