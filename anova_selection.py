5427475b421bf6b682620fe2c7746e720556c0f3
Moyne Nicolas <nicolas.moyne@student-cs.fr>
2023-10-24 19:03:52 +0200

    saving fscore

--- anova_selection.py
+++ anova_selection.py
@@ -4,27 +4,33 @@ def square(mat):
     m = np.mean(mat, axis = 0)
 
 def anova_f(x, y, k = 20):
-    nb_data, nb_feature = np.shape(x)
-    overall_mean = np.mean(y)
-    ss = []
-    df = []
-    ms = []
-    for feature in range(nb_feature):
-        print(feature)
-        categories = np.unique(x[:,feature])
-        means_categ = {c: np.mean(y[x[:,feature] == c]) for c in categories}
-        ss_c = np.sum([(means_categ[c] - overall_mean) ** 2 * sum(x[:,feature] == c) for c in categories])
-        ss.append(ss_c)
-        df_c = len(categories) - 1
-        df.append(df_c)
-        ms_c = ss_c/df_c
-        ms.append(ms_c)
-    ss_error = np.sum((y - overall_mean) ** 2 - np.sum(ss))
-    df_error = len(y) - np.sum(df) + 1
-    ms_error = ss_error/df_error
-    f_stat = []
-    for feature in range(nb_feature):
-        f_stat.append(ms[feature]/ms_error)
+    try:
+        f_stat = list(np.loadtxt("f_scores.csv"))
+    except:
+        print("Creating the F-score file - this might take a while")
+        nb_data, nb_feature = np.shape(x)
+        overall_mean = np.mean(y)
+        ss = []
+        df = []
+        ms = []
+        for feature in range(nb_feature):
+            print(feature)
+            categories = np.unique(x[:,feature])
+            means_categ = {c: np.mean(y[x[:,feature] == c]) for c in categories}
+            ss_c = np.sum([(means_categ[c] - overall_mean) ** 2 * sum(x[:,feature] == c) for c in categories])
+            ss.append(ss_c)
+            df_c = len(categories) - 1
+            df.append(df_c)
+            ms_c = ss_c/df_c
+            ms.append(ms_c)
+        ss_error = np.sum((y - overall_mean) ** 2 - np.sum(ss))
+        df_error = len(y) - np.sum(df) + 1
+        ms_error = ss_error/df_error
+        f_stat = []
+        for feature in range(nb_feature):
+            f_stat.append(ms[feature]/ms_error)
+        
+        np.savetxt("f_scores.csv", f_stat, delimiter=",")
     
     top_k_indices = np.argpartition(f_stat, -k)[-k:]
 
