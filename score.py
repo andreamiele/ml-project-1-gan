6fc489d56efc7245dd1969430c05bbc49fbe7a20
Andrea Miele <andrea.miele.pro@gmail.com>
2023-10-26 17:34:30 +0200

    m

--- score.py
+++ score.py
@@ -3,13 +3,10 @@ import numpy as np
 
 def get_confusion_matrix(y_true, y_pred):
     # Calculate True Negatives (TN)
-    TN = ((y_true == 0) & (y_pred == 0)).sum()
-    # Calculate True Positives (TP)
-    TP = ((y_true == 1) & (y_pred == 1)).sum()
-    # Calculate False Negatives (FN)
-    FN = ((y_true == 1) & (y_pred == 0)).sum()
-    # Calculate False Positives (FP)
-    FP = ((y_true == 0) & (y_pred == 1)).sum()
+    TP = np.sum((y_true == 1) & (y_pred == 1))
+    TN = np.sum((y_true == -1) & (y_pred == -1))
+    FN = np.sum((y_true == 1) & (y_pred == -1))
+    FP = np.sum((y_true == -1) & (y_pred == 1))
     # Return the confusion matrix as a 2x2 numpy array
     return np.array([[TN, FP], [FN, TP]])
 
@@ -22,9 +19,22 @@ def accuracy_score(y_true, y_pred):
 
 
 def f1_score(y_true, y_pred):
-    TN, FP, FN, TP = get_confusion_matrix(y_true, y_pred).ravel()
-    # Calculate precision score
-    precision_score, recall_score = TP / (TP + FP), TP / (TP + FN)
-    # Calculate f1-score
-    f1_score = (2 * precision_score * recall_score) / (precision_score + recall_score)
-    return f1_score
+    # Ensure inputs are NumPy arrays
+    y_true = np.array(y_true)
+    y_pred = np.array(y_pred)
+
+    # Calculate True Positives, False Positives, and False Negatives
+    true_positives = np.sum((y_true == 1) & (y_pred == 1))
+    false_positives = np.sum((y_true == -1) & (y_pred == 1))
+    false_negatives = np.sum((y_true == 1) & (y_pred == -1))
+
+    # Calculate precision and recall
+    precision = true_positives / (true_positives + false_positives)
+    recall = true_positives / (true_positives + false_negatives)
+    if precision + recall == 0:
+        f1 = 0
+    else:
+        # Calculate F1 score
+        f1 = 2 * (precision * recall) / (precision + recall)
+
+    return f1
